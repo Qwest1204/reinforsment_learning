2d9bf3d6d99f75a13af6e023fedb95a923385339
Qwest1204 <myPCwithUbuntu@gmail.com>
2024-12-01 10:37:10 +0300

    feat(MDN-RNN): add MDN neural network

--- MDN_RNN/model.py
+++ MDN_RNN/model.py
@@ -9,52 +9,77 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 def detach(states):
     return [state.detach() for state in states] 
 
-class V(nn.Module):
-    def __init__(self, nembed, nhidden=265, nlayers=1):
-        super(V, self).__init__()
+class MDNRNN(nn.Module):
+    def __init__(self, z_size, n_hidden=256, n_gaussians=5, n_layers=1):
+        super(MDNRNN, self).__init__()
 
-        self.nhidden = nhidden
-        self.nlayers = nlayers
+        self.z_size = z_size
+        self.n_hidden = n_hidden
+        self.n_gaussians = n_gaussians
+        self.n_layers = n_layers
         
-        self.lstm = nn.LSTM(nembed, nhidden, nlayers, batch_first=True)
-        self.linear = nn.Linear(nhidden, nembed)
+        self.lstm = nn.LSTM(z_size, n_hidden, n_layers, batch_first=True)
+        self.fc1 = nn.Linear(n_hidden, n_gaussians*z_size)
+        self.fc2 = nn.Linear(n_hidden, n_gaussians*z_size)
+        self.fc3 = nn.Linear(n_hidden, n_gaussians*z_size)
+        
+    def get_mixture_coef(self, y):
+        rollout_length = y.size(1)
+        pi, mu, sigma = self.fc1(y), self.fc2(y), self.fc3(y)
+        
+        pi = pi.view(-1, rollout_length, self.n_gaussians, self.z_size)
+        mu = mu.view(-1, rollout_length, self.n_gaussians, self.z_size)
+        sigma = sigma.view(-1, rollout_length, self.n_gaussians, self.z_size)
+        
+        pi = F.softmax(pi, 2)
+        sigma = torch.exp(sigma)
+        return pi, mu, sigma
+        
         
     def forward(self, x, h):
         # Forward propagate LSTM
-        out, (h, c) = self.lstm(x, h)
-        out = self.linear(out)
-        return out, (h, c)
+        y, (h, c) = self.lstm(x, h)
+        pi, mu, sigma = self.get_mixture_coef(y)
+        return (pi, mu, sigma), (h, c)
     
     def init_hidden(self, bsz):
-        return (torch.zeros(self.nlayers, bsz, self.nhidden).to(DEVICE),
-                torch.zeros(self.nlayers, bsz, self.nhidden).to(DEVICE))
-    
-def train_RNN(epochs, seqlen, model, z, ):
+        return (torch.zeros(self.n_layers, bsz, self.n_hidden).to(DEVICE),
+                torch.zeros(self.n_layers, bsz, self.n_hidden).to(DEVICE))
 
-    criterion = nn.MSELoss()
+def mdn_loss_fn(y, pi, mu, sigma):
+    m = torch.distributions.Normal(loc=mu, scale=sigma)
+    loss = torch.exp(m.log_prob(y))
+    loss = torch.sum(loss * pi, dim=2)
+    loss = -torch.log(loss)
+    return loss.mean()
+
+def criterion(y, pi, mu, sigma):
+    y = y.unsqueeze(2)
+    return mdn_loss_fn(y, pi, mu, sigma)
+
+def train_RNN(epochs, seqlen, model, z, bsz):
     optimizer = torch.optim.Adam(model.parameters())
     # Train the model
     for epoch in range(epochs):
         # Set initial hidden and cell states
-        hidden = model.init_hidden(32)
-        loss = 0
-        for i in range(0, 257, seqlen):
+        hidden = model.init_hidden(bsz)
+        
+        for i in range(0, 8, 16):
             # Get mini-batch inputs and targets
             inputs = z[:, i:i+seqlen, :].to(DEVICE)
             targets = z[:, (i+1):(i+1)+seqlen, :].to(DEVICE)
             
             # Forward pass
             hidden = detach(hidden)
-            outputs, hidden = model(inputs, hidden)
-            #print(outputs.shape, targets.shape)
-            loss = criterion(outputs, targets)
+            (pi, mu, sigma), hidden = model(inputs, hidden)
+            loss = criterion(targets, pi, mu, sigma)
             
             # Backward and optimize
             model.zero_grad()
             loss.backward()
-            clip_grad_norm_(model.parameters(), 0.5)
+            # clip_grad_norm_(model.parameters(), 0.5)
             optimizer.step()
             
-        if epoch % 50 == 0:
-            print ('Epoch [{}/{}], Loss: {:.10f}'
-                .format(epoch+1, epochs, loss))
+        if epoch % 100 == 0:
+            print ('Epoch [{}/{}], Loss: {:.4f}'
+                .format(epoch, epochs, loss.item()))
