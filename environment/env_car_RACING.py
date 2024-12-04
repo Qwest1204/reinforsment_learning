77c04d93694abd4969cd8c0be9e789f975a78888
Qwest1204 <myPCwithUbuntu@gmail.com>
2024-12-04 23:24:08 +0300

    start before competition

--- environment/env_car_RACING.py
+++ environment/env_car_RACING.py
@@ -1,28 +1,15 @@
-import gym
-from gym.envs.box2d import CarRacing
+import gymnasium as gym
+from gymnasium.wrappers import RecordEpisodeStatistics, RecordVideo
+from gymnasium.utils.play import play
+num_eval_episodes = 4
 
-from stable_baselines3.common.vec_env import DummyVecEnv
-from stable_baselines3 import PPO
+env = gym.make("CarRacing-v3", render_mode="rgb_array")  # replace with your environment
+env = RecordVideo(env, video_folder="cartpole-agent", name_prefix="eval",
+                  episode_trigger=lambda x: True)
+env = RecordEpisodeStatistics(env, buffer_length=num_eval_episodes)
 
-if __name__=='__main__':
-    env = lambda :  CarRacing(
-        #grayscale=1,
-        #show_info_panel=0,
-        #discretize_actions="hard",
-        #frames_per_state=4,
-        #num_lanes=1,
-        #num_tracks=1,
-        )
+play(env)
 
-    #env = getattr(environments, env)
-    env = DummyVecEnv([env])
-
-    model = PPO.load('/home/qwest/project/PycharmProjects/Reinforsment_Learning/environment/car_racing_weights.pkl.zip')
-
-    model.set_env(env)
-
-    obs = env.reset()
-    while True:
-        action, _states = model.predict(obs)
-        obs, rewards, dones, info = env.step(action)
-        env.render()
+print(f'Episode time taken: {env.time_queue}')
+print(f'Episode total rewards: {env.return_queue}')
+print(f'Episode lengths: {env.length_queue}')
